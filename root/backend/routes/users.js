5e17c0d116c86bc1eedc54e7a31bdfa87f989228
Marco Aguilar <aguilaramarco@yahoo.com>
2019-07-17 13:40:42 -0700

    Router inside users.js now uses bcrypt to compare hashed password inside database with password entered from user when attempting to log in.

--- root/backend/routes/users.js
+++ root/backend/routes/users.js
@@ -8,14 +8,14 @@ const bcrypt = require('bcrypt');
 // https://stackoverflow.com/questions/46693430/what-are-salt-rounds-and-how-are-salts-stored-in-bcrypt
 const saltRounds = 10;
 
-
+/**
+ * Create user
+ * 
+ * For now we'll assume each user created is unique.
+ * Later, we need to check to make sure the username doesn't exists.
+ */
 // Create user
 router.post('/register/:username/:password', function(req,res) {
-
-    // For now we'll assume each user created is unique
-    // Later, we need to check to make sure the username doesn't exists.
-
-
     // Hash password
     const plainPassword = req.params.password;
     bcrypt.hash(plainPassword, saltRounds,
@@ -25,6 +25,8 @@ router.post('/register/:username/:password', function(req,res) {
                 next(err);
             }
             else {
+                console.log("Hashed Password before storing");
+                console.log(hashedPassword);
                 // Store user into database.
                 const newUser = {username: req.params.username, password: hashedPassword};
                 _db.collection("users").insertOne(newUser, function(err, result) {
@@ -53,19 +55,42 @@ router.post('/:username/:password', function (req, res) {
          * https://stackoverflow.com/questions/7042340/error-cant-set-headers-after-they-are-sent-to-the-client
          */
         if(result.length === 0) {
+            // User not found!
             res.statusCode = 404;
             return;
         }
         
         /**
-         * Take the password in the database, unhash it, and compare it to the 
-         * password passed in from the form.
+         * username found, now check password.
+         * 
+         * Compare password that was sent by the user (1st arg)
+         * with the hashed password in the database (2nd arg)
          */
-        if(result[0].password === req.params.password) {
-            res.send(true);
-        } else {
-            res.send(false);
-        }      
+        console.log("Comparing...");
+        console.log(req.params.password);
+        bcrypt.compare(req.params.password, result[0].password, function(err, same) {
+            if (err) {
+                console.log(err);
+                res.status(500).json({
+                    error: 'Internal error please try again'
+                });
+            } else if (!same){
+                console.log("Not the same!");
+                res.status(401)
+                .json({
+                  error: 'Incorrect email or password'
+                });
+            } else {
+                // Issue Token
+                console.log("Correct username and password! Here's your token!");
+            }
+        });
+
+        // if(result[0].password === req.params.password) {
+        //     res.send(true);
+        // } else {
+        //     res.send(false);
+        // }      
     });
 });
 
